06453ba29c2bd2c881ad37f6faecad274a3f6755
glicOne <romaniuk.cv@gmail.com>
2018-04-21 21:48:49 +0300

    Add json struct

--- src/gelbooru_api.erl
+++ src/gelbooru_api.erl
@@ -52,3 +52,22 @@ get_photo({Pid,Tag}) ->
 %%    # there was some other error, e.g. server is not available
 %%    {:error, {:network, reason}}
 %% end
+
+%% response json struct {
+%%	Directory    string      `json:"directory"`
+%%	Hash         string      `json:"hash"`
+%%	Height       int         `json:"height"`
+%%	ID           int         `json:"id"`
+%%	Image        string      `json:"image"`
+%%	Change       int         `json:"change"`
+%%	Owner        string      `json:"owner"`
+%%	ParentID     interface{} `json:"parent_id"`
+%%	Rating       string      `json:"rating"`
+%%	Sample       bool        `json:"sample"`
+%%	SampleHeight int         `json:"sample_height"`
+%%	SampleWidth  int         `json:"sample_width"`
+%%	Score        int         `json:"score"`
+%%	Tags         string      `json:"tags"`
+%%	Width        int         `json:"width"`
+%%	FileURL      string      `json:"file_url"`
+%%}
