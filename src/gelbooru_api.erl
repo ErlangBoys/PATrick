72259f61bed7e7125a99edd40a3a575e25695821
glicOne <romaniuk.cv@gmail.com>
2018-04-21 21:06:12 +0300

    Working gelbooru_api

--- src/gelbooru_api.erl
+++ src/gelbooru_api.erl
@@ -12,20 +12,30 @@
 %% id The post id.
 %% json Set to 1 for JSON formatted response.
 -record(gelbooru_schema, {url = <<"https://gelbooru.com/">>,
-			  path = <<"index.php?page=dapi&s=post&q=index&json=1&limit=100">>,
+			  path = <<"index.php">>,
+			  page = <<"dapi">>,
+			  s = <<"post">>,
+			  q = <<"index">>,
+			  json = <<"1">>,
+			  limit = <<"100">>,
 			  pid, tag, id}).
 
 get_photo({Pid,Tag}) ->
 %%  hackney_url:qs([{<<"none">>,<<"pror">>},{<<"none">>,<<"por">>}]).  
 %% <<"none=pror&none=por">>
 
-    GConf = #gelbooru_schema{pid= <<Pid>>,
-			     tag= <<Tag>>
+    GConf = #gelbooru_schema{pid=Pid,
+			     tag=Tag
 			    },
 
     URL = hackney_url:make_url(GConf#gelbooru_schema.url,
 			       GConf#gelbooru_schema.path,
-			       [{<<"pid">>, <<(GConf#gelbooru_schema.pid)/binary>>},
+			       [{<<"page">>, <<(GConf#gelbooru_schema.page)/binary>>},
+				{<<"s">>, <<(GConf#gelbooru_schema.s)/binary>>},
+				{<<"q">>, <<(GConf#gelbooru_schema.q)/binary>>},
+				{<<"json">>, <<(GConf#gelbooru_schema.json)/binary>>},
+				{<<"limit">>, <<(GConf#gelbooru_schema.limit)/binary>>},
+				{<<"pid">>, <<(GConf#gelbooru_schema.pid)/binary>>},
 				{<<"tags">>, <<(GConf#gelbooru_schema.tag)/binary>>}]),
     
     {ok, _ResultCode, _Result, ClientRef} = hackney:request(get, URL),
