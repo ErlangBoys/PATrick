b25e8811696d04830d8a70ec8638d33161bbdd27
glicOne <romaniuk.cv@gmail.com>
2018-04-22 03:21:39 +0300

    Fixed bad function arity

--- src/send_to_telegram.erl
+++ src/send_to_telegram.erl
@@ -1,5 +1,7 @@
 -module(send_to_telegram).
 -include_lib("common_test/include/ct.hrl"). %% For test working
 
--export(""). %% TODO add functions and args
+-export([send/1]). %% TODO add functions and args
 
+send(_File) ->
+	ok.
