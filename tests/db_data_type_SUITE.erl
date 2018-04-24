bf3120458104efdfe19ca7993540ed778725214f
Лиференко Павел <liferenko@reg.ru>
2018-04-24 20:31:31 +0300

    initialize some db_tests before working with postgres

--- tests/db_data_type_SUITE.erl
+++ tests/db_data_type_SUITE.erl
@@ -3,13 +3,21 @@
 
 -module(db_data_type_SUITE).
 
+-include_lib("eunit/include/eunit.hrl").
 -include_lib("common_test/include/ct.hrl").
+-include("epgsql.hrl").
+
+connect_test(Module) ->
+    connect_only(Module, []).
+
+connect_to_db_test(Module) ->
+    connect_only(Module, [{database, "epgsql_test_db"}]).
 
  %% Test server callbacks
--export([suite/0, all/0, 
-     init_per_suite/1, end_per_suite/1,
-     init_per_testcase/2, end_per_testcase/2]).
+%% -export([suite/0, all/0, 
+%%     init_per_suite/1, end_per_suite/1,
+%%     init_per_testcase/2, end_per_testcase/2]).
 
 %% Test cases
--export([string/1, integer/1]).
--define(CONNECT_STR, "DSN=sqlserver;UID=alladin;PWD=sesame"). %% TODO change DSN UID PWD
+%% -export([string/1, integer/1]).
+%% -define(CONNECT_STR, "DSN=sqlserver;UID=alladin;PWD=sesame"). %% TODO change DSN UID PWD
---
+++ tests/imageboards_api_SUITE.erl
@@ -0,0 +1,5 @@
+-module(imageboards_api_SUITE).
+-include_lib("common_test/include/ct.hrl"). 
+
+
+%% Test cases
