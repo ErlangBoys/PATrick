884082bca91f31f4ac938e61a530853eed2e1d04
Лиференко Павел <liferenko@reg.ru>
2018-04-28 15:05:22 +0300

    initialize Postgres (pg_db), add db logs

--- src/db_connect.erl
+++ src/db_connect.erl
@@ -14,7 +14,7 @@
         -> {ok, Connection :: connection()} | {error, Reason :: connect_error()}.    
 
 
-
+%% START / maybe it should be in another module 
 connect(Host, Username, Password, Opts) -> 
       {ok, C} = epgsqla:start_link(),
     Ref = epgsqla:connect(C, "localhost", "username", "psss", [{database, "db_for_api_data"}]),
@@ -26,3 +26,37 @@ connect(Host, Username, Password, Opts) ->
         {'EXIT', C, _Reason} ->
             {error, closed}
     end.
+
+
+%% END / maybe it should be in another module 
+
+
+%% START / maybe it should be in another module 
+-type query() :: string() | iodata().
+-type squery_row() :: {binary()}.
+
+-record(column, {
+    name :: binary(),
+    type :: epgsql_type(),
+    size :: -1 | pos_integer(),
+    modifier :: -1 | pos_integer(),
+    format :: integer()
+}).
+
+-type ok_reply(RowType) ::
+    {ok, ColumnsDescription :: [#column{}], RowsValues :: [RowType]} |                            % select
+    {ok, Count :: non_neg_integer()} |                                                            % update/insert/delete
+    {ok, Count :: non_neg_integer(), ColumnsDescription :: [#column{}], RowsValues :: [RowType]}. % update/insert/delete + returning
+-type error_reply() :: {error, query_error()}.
+-type reply(RowType) :: ok_reply() | error_reply().
+
+-spec squery(connection(), query()) -> reply(squery_row()) | [reply(squery_row())].
+%% @doc runs simple `SqlQuery' via given `Connection'
+squery(Connection, SqlQuery) -> 
+    epgsql:squery(C, "insert into account (name) values  ('alice'), ('bob')").
+
+%% END / maybe it should be in another module 
+
+
+%% START / maybe it should be in another module 
+%% ENDa / maybe it should be in another module 
